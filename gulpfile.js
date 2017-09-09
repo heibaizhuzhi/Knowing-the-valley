fe71e1d649f3715cee000fb59e86cf7906a55194
heibai <1826524919@qq.com>
2017-09-09 12:31:26 +0800

    浏览器环境测试

--- gulpfile.js
+++ gulpfile.js
@@ -54,29 +54,92 @@ gulp.task("html",function(){
 // });
 
 // 编写一个打包工具
-	// gulp.task("js",function(){
-	// 	browserify("src/js/index.js").bundle()
-	// 		.pipe(source("index.js"))
-	// 		.pipe(buffer())
-	// 		.pipe(gulp(gulp.dest("dist/js")));
-	// });
+// gulp.task('js', function() {
+//     browserify('src/js/index.js').bundle() // 打包index.js
+//         .pipe(source('index.js'))
+//         .pipe(buffer())
+//         // .pipe(uglify())
+//         .pipe(gulp.dest('dist/js'));
+// });
 
-gulp.task('js', function() {
-    browserify('src/js/index.js').bundle() // 打包index.js
-        .pipe(source('index.js'))
-        .pipe(buffer())
-        // .pipe(uglify())
+// 配置要打包的第三包路径
+var jsLibs = [
+    'node_modules/art-template/lib/template-web.js',
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'node_modules/jquery-form/dist/jquery.form.min.js'
+];
+// 合并所有的第三方包为一个js
+gulp.task('jsLib', function() {
+    gulp.src(jsLibs)
+        .pipe(concat('lib.js'))
         .pipe(gulp.dest('dist/js'));
 });
 
+var jsModules = [
+    // 首页
+    'src/js/index.js',
+    // 用户
+    'src/js/user/login.js',
+    'src/js/user/repass.js',
+    'src/js/user/profile.js',
+    // 讲师
+    'src/js/teacher/add.js',
+    'src/js/teacher/edit.js',
+    'src/js/teacher/list.js',
+    // 课程
+    'src/js/course/add.js',
+    'src/js/course/edit1.js',
+    'src/js/course/edit2.js',
+    'src/js/course/edit3.js',
+    'src/js/course/list.js',
+    // 学科分类
+    'src/js/category/add.js',
+    'src/js/category/edit.js',
+    'src/js/category/list.js'
+];
+
+// gulp.task("js",function(){
+// 	jsModules.forEach(function(jsPath) {
+// 		var pathArr = jsPath.split("/");
+// 		var jsName =pathArr.pop();
+// 		pathArr.shift();
+// 		browserify(jsPath,{debug:true}).bundle()
+// 			.pipe(source(jsName))
+// 			.pipe(buffer)
+// 			.pipe(gulp.dest("dist/"+pathArr.join("/")));
+// 	});
+// });
+
+gulp.task('js', function() {
+    jsModules.forEach(function(jsPath) {
+        var pathArr = jsPath.split('/'); // jsPath变成['src', 'js', 'user', 'login.js']
+        var jsName = pathArr.pop(); // 取出login.js，数组变成['src', 'js', 'user']
+        pathArr.shift(); // 取出src，数组变成['js', 'user']
+        browserify(jsPath, { debug: true }).bundle() // 打包index.js
+            .pipe(source(jsName))
+            .pipe(buffer())
+            // .pipe(uglify())
+            .pipe(gulp.dest('dist/' + pathArr.join('/'))); // 数组变成'js/user'
+    });
+});
+
+// 添加统一打包的任务
+gulp.task('build', function() {
+    gulp.run(['html', 'less', 'jsLib', 'js']);
+});
+
 gulp.task("default",function(){
+
+	gulp.run("build");
+
 	gulp.run(["less","html","js"]);
 
 	gulp.watch("src/less/*.js",function(){
 		gulp.run("less");
 	});
 
-	gulp.watch("src/**/*.html",function(){
+	gulp.watch(["src/**/*.html","index.html"],function(){
 		gulp.run("html");
 	});
 
